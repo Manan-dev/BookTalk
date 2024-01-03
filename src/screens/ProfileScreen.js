89c0c6aa59f96a9022c8f3a42baff6f0e58af2c4
manan-dev <mpatel65@vols.utk.edu>
2024-01-02 20:39:05 -0500

    WIP: Profile Screen

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,12 +1,30 @@
+import { Ionicons } from '@expo/vector-icons';
 import { Button } from '@rneui/themed';
-import React, { useContext } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useContext, useEffect, useState } from 'react';
+import {
+	ScrollView,
+	StyleSheet,
+	Text,
+	TextInput,
+	TouchableOpacity,
+	View,
+} from 'react-native';
+import { SafeAreaView } from 'react-navigation';
+import Carousel from '../components/Carousel';
 import { FirebaseContext } from '../context/FirebaseContext';
 import { UserContext } from '../context/UserContext';
+import booksReadData from '../data/booksRead.json';
+import futureBooksData from '../data/futureBooks.json';
+import mysteryBooksData from '../data/mysteryBooks.json';
 
 export default function ProfileScreen() {
 	const [user, setUser] = useContext(UserContext);
 	const firebase = useContext(FirebaseContext);
+	const [data, setData] = useState([]);
+
+	useEffect(() => {
+		setData(booksReadData);
+	}, []);
 
 	handleLogout = async () => {
 		const loggedOut = await firebase.logout();
@@ -16,27 +34,98 @@ export default function ProfileScreen() {
 		}
 	};
 	return (
-		<View style={styles.container}>
-			<Text>Username: {user.username}</Text>
-			<Text>Email: {user.email}</Text>
+		<ScrollView contentContainerStyle={styles.container}>
+			<SafeAreaView style={styles.content}>
+				<TouchableOpacity>
+					<Ionicons
+						name="ios-person-circle-outline"
+						size={150}
+						color="black"
+						style={styles.profilePicIcon}
+					/>
+				</TouchableOpacity>
+				<Text style={styles.username}>{user.username}</Text>
+				<View style={styles.ffContainer}>
+					<View>
+						<Text style={styles.count}>425</Text>
+						<Text>Followers</Text>
+					</View>
+					<View>
+						<Text style={styles.count}>437</Text>
+						<Text>Following</Text>
+					</View>
+				</View>
+				<TextInput
+					multiline
+					numberOfLines={4}
+					placeholder="Bio..."
+					style={styles.bio}
+				></TextInput>
 
-			<Button buttonStyle={styles.logoutButton} onPress={handleLogout}>
-				Log Out
-			</Button>
-		</View>
+				<View>
+					<Carousel carouselData={booksReadData} title="Books Read" />
+				</View>
+				<View>
+					<Carousel
+						carouselData={mysteryBooksData}
+						title="Best Mystery Books"
+					/>
+				</View>
+				<View>
+					<Carousel
+						carouselData={futureBooksData}
+						title="Books I Want to Read"
+					/>
+				</View>
+				<Button buttonStyle={styles.logoutButton} onPress={handleLogout}>
+					Log Out
+				</Button>
+			</SafeAreaView>
+		</ScrollView>
 	);
 }
 
 const styles = StyleSheet.create({
 	container: {
-		flex: 1,
+		flexGrow: 1,
 		backgroundColor: '#fff',
 		alignItems: 'center',
-		justifyContent: 'center',
+	},
+	content: {
+		width: '100%',
+		alignItems: 'center',
+		marginTop: 50,
 	},
 	logoutButton: {
 		width: 'auto',
 		borderRadius: 10,
 		alignSelf: 'center',
+	},
+	profilePicIcon: {
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	username: {
+		fontSize: 25,
+		marginBottom: 20,
+	},
+	ffContainer: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		width: '80%',
+		marginBottom: 20,
+	},
+	count: {
+		fontSize: 30,
+		fontWeight: 'bold',
+	},
+	bio: {
+		width: '90%',
+		fontSize: 18,
+		minHeight: 90,
+		maxHeight: 90,
+		marginBottom: 20,
+		borderWidth: 1,
+		backgroundColor: 'rgba(0,0,0,0.1)',
 	},
 });
