f8c740c2a415856d09d92792e158f25241faff32
manan-dev <mpatel65@vols.utk.edu>
2024-01-05 20:32:20 -0500

    Create custom header component

---
+++ src/components/CustomHeader.js
@@ -0,0 +1,42 @@
+import { Kings_400Regular, useFonts } from '@expo-google-fonts/kings';
+import { useNavigation } from '@react-navigation/native';
+import { Text } from '@rneui/themed';
+import React from 'react';
+import { SafeAreaView, StyleSheet, TouchableOpacity } from 'react-native';
+
+const CustomHeader = () => {
+	const navigation = useNavigation();
+	let [fontsLoaded] = useFonts({
+		Kings_400Regular,
+	});
+
+	if (!fontsLoaded) {
+		return null;
+	}
+	return (
+		<SafeAreaView style={styles.container}>
+			<TouchableOpacity onPress={() => navigation.navigate('Home')}>
+				<Text h3 h3Style={styles.logoText}>
+					BookTalk
+				</Text>
+			</TouchableOpacity>
+		</SafeAreaView>
+	);
+};
+
+export default CustomHeader;
+
+const styles = StyleSheet.create({
+	container: {
+		backgroundColor: '#fff',
+		borderBottomWidth: 1,
+		borderBottomColor: '#ddd',
+	},
+	logoText: {
+		fontSize: 30,
+		color: '#E9446A',
+		fontFamily: 'Kings_400Regular',
+		marginLeft: 15,
+		marginTop: 10,
+	},
+});
