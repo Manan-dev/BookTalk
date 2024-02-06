d95878c68cfc53764f02a8279f7a33426ee99654
tulsitailor <tulsitailor01@gmail.com>
2024-02-06 10:55:13 -0500

    Added a searchbar on searchscreen

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import SearchBar from '../components/SearchBar';   // Import the SearchBar component
 
+// Functional component for the SearchScreen
 const SearchScreen = () => {
+
+	// Define a function to handle the search action
+	const handleSeach = (query) => {
+		console.log('Search Query: ', query)
+		// Upadate state or make API calls here
+	};
+
+	// Render the SearchScreen component
 	return (
 		<View style={styles.container}>
-			<Text>Search Screen</Text>
+
+			<SearchBar onSearch={handleSeach} />
+
+			{/* Content for the SearchScreen goes here */}
+			<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+				{/* <Text>Search Screen</Text> */}
+			</View>
 		</View>
 	);
 };
